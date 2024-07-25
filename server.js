bbe08b19c79652f077163284307a06ef3f31ef9d
momokokong <momokokong@gmail.com>
2024-07-25 13:14:16 -0700

    use writeFileSync and add error check to POST and DELETE

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 // This is a Note Taker server handles http requests and interact with db.json to read, write and delete notes.  
 // required modules: express, path, short-unique-id, ./helpers/fsUtils.js
+// note thawt writeToFile is rewritten with writeFileSync.
 const express = require("express");
 const path = require("path");
 const { readFromFile, writeToFile, readAndAppend } = require("./helpers/fsUtils");
@@ -41,30 +42,39 @@ app.get("/api/notes", (req, res) => {
 
 // route serverURL/api/notes POST request to add a new note with UUID to db.json.
 app.post("/api/notes", (req, res) => {
-  const newNotes = {
-    ...req.body, 
-    id: uid.rnd()
-  };
-  readAndAppend(newNotes, db)
-  res.end();
+  const {title, text} = req.body;
+  if (title && text){
+    const newNotes = {
+      ...req.body, 
+      id: uid.rnd()
+    };
+    readAndAppend(newNotes, db)
+    res.status(200).end();
+  } else {
+    res.status(400).json("400 bad request missing title or text from req");
+  }
 });
 
 // route serverURL/api/notes/:id DELETE request to remove a post associated with a specific UUID from db.json.  
-// If UUID is not found, it does not make any change to the db.
+// If UUID is not found, it does nothing to db.json and return an error code 400 back to the client.
 app.delete("/api/notes/:id", (req ,res) => {
   readFromFile(db)
   .then((data) => {
     const savedNotes = JSON.parse(data);
     const delIndex = savedNotes.findIndex((note) => note.id === req.params.id);
-    if (delIndex != -1) savedNotes.splice(delIndex, 1);
+    if (delIndex == -1) throw new Error("400 bad request id " + req.params.id + " not found.");  
+    savedNotes.splice(delIndex, 1);
     return savedNotes;
   }).then((newNotes) => {
     writeToFile(db, newNotes);
-    res.end();
-  });
+    res.status(200).end();
+  }).catch((err) => {
+    res.status(400).json(err.message);
+});
+
 });
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} if running from local`);
+  console.log(`App listening at http://localhost:${PORT} if running on local`)
 );
 
